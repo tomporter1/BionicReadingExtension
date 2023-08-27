374946e0749c068633faa70cc245a2ebcdbb4f2e
Tom Porter <tomporter62@gmail.com>
2023-08-27 13:50:14 +0100

    Added missing files from the manifest

--- content.js
+++ content.js
@@ -1,1 +1,1 @@
-document.body.innerHTML = document.body.innerHTML.replace(/\b(\w{1,2})/g, '<strong>$1</strong>');
+document.body.innerHTML = document.body.innerHTML.replace(/\b(\w{1,2})/g, '<strong class="bionic-bold">$1</strong>');
