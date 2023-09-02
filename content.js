0b16427d949e52fa3c06293a47bb6b8046ac3d41
Tom Porter <tomporter62@gmail.com>
2023-09-02 15:04:37 +0100

    Updated to run as new content is added to page

--- content.js
+++ content.js
@@ -1,17 +1,26 @@
-const strength = 0.4
+const strength = 0.4;
 
+/**
+ * Bold the text in a given node.
+ * @param {Node} node - The node to process.
+ */
 function boldTextInNode(node) {
-    if (node.nodeType === Node.TEXT_NODE) {
+    //null check on node
+    if (!node) {
+        return;
+    }
+    if (node.nodeType === Node.TEXT_NODE && node.parentNode && node.parentNode.classList && !node.parentNode.classList.contains('bionic-processed')) {
         // Check if the node has actual visible text
         if (/\S/.test(node.nodeValue)) {
             const wrapper = document.createElement('span');
+            wrapper.className = 'bionic-processed';
             wrapper.innerHTML = node.nodeValue.replace(/\b(\w+)\b/g, function (match, word) {
                 const boldLength = Math.ceil(word.length * strength);
                 return `<strong class="bionic-bold">${word.substring(0, boldLength)}</strong>${word.substring(boldLength)}`;
             });
             node.replaceWith(wrapper);
         }
-    } else if (node.nodeType === Node.ELEMENT_NODE) {
+    } else if (node.nodeType === Node.ELEMENT_NODE && node.parentNode && node.parentNode.classList && !node.classList.contains('bionic-processed')) {
         // If it's an element node, iterate over its children
         for (let child of node.childNodes) {
             boldTextInNode(child);
@@ -19,15 +28,12 @@ function boldTextInNode(node) {
     }
 }
 
-// console.log("Sending message to background script");
-// chrome.runtime.sendMessage({ action: "isDomainBlocked", domain: window.location.hostname }, (response) => {
-//     console.log("Received response:", response);
-//     if (!response.isBlocked) {
-//         boldTextInNode(document.body);
-//     }
-// });
-
-// Function to check if the domain is in the CSV list
+/**
+ * Checks if a given domain is present in a list of domains.
+ * @param {string} domain - The domain to check.
+ * @param {string[]} list - The list of domains to check against.
+ * @returns {boolean} - True if the domain is present in the list, false otherwise.
+ */
 function isDomainInList(domain, list) {
     return list.includes(domain);
 }
@@ -61,10 +67,24 @@ fetch(chrome.runtime.getURL("blocklist.csv"))
             blockedDomains.push(line);
         });
         console.log(`Loaded ${blockedDomains.length} domains to block.`);
-        
+
         // Call the function after blocklist is loaded
         checkDomainAndRunExtension();
     })
     .catch(error => {
         console.log("Fetch error: " + error);
     });
+
+// MutationObserver to watch for changes in the DOM
+const observer = new MutationObserver((mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            for (let addedNode of mutation.addedNodes) {
+                boldTextInNode(addedNode);
+            }
+        }
+    }
+});
+
+// Start observing the entire document with the configured parameters
+observer.observe(document, { childList: true, subtree: true });
